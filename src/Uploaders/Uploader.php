c3d8da357f608f13bb0458e6bec955be59ee92ab
Pedro X <pxpm88@gmail.com>
2023-03-08 13:47:13 +0000

    fix url retrival

--- src/Uploaders/Uploader.php
+++ src/Uploaders/Uploader.php
@@ -62,6 +62,13 @@ abstract class Uploader implements UploaderInterface
 
     public function retrieveUploadedFile(Model $entry)
     {
+        $value = $entry->{$this->fieldName};
+
+        if($this->isMultiple && !isset($entry->getCasts()[$this->fieldName]) && is_string($value)) {
+            $entry->{$this->fieldName} = json_decode($value, true);
+        }else{
+            $entry->{$this->fieldName} = Str::after($value, $this->path);
+        }
         return $entry;
     }
 
