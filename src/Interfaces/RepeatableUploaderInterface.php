7c4dc6433ddf011db2927f2a30a445f92f1c690b
Pedro X <pxpm88@gmail.com>
2023-03-09 14:24:35 +0000

    cleanup interface methods

--- src/Interfaces/RepeatableUploaderInterface.php
+++ src/Interfaces/RepeatableUploaderInterface.php
@@ -2,7 +2,15 @@
 
 namespace Backpack\MediaLibraryUploads\Interfaces;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface RepeatableUploaderInterface
 {
-    public function uploads(... $uploads): self;
+    public function uploads(...$uploads): self;
+
+    public static function for(array $field): self;
+
+    public function processFileUpload(Model $entry);
+
+    public function retrieveUploadedFile(Model $entry);
 }
